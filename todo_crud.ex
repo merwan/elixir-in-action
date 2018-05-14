6ab35edfe13971961aec03ecc11b7d67e60e31e0
Merouane Atig <merwan@patem.net>
2018-05-14 18:28:12 +0200

    Implement TodoList with struct

---
+++ todo_crud.ex
@@ -0,0 +1,27 @@
+defmodule TodoList do
+  defstruct auto_id: 1, entries: %{}
+
+  def new(), do: %TodoList{}
+
+  def add_entry(todo_list, entry) do
+    entry = Map.put(entry, :id, todo_list.auto_id)
+
+    new_entries = Map.put(todo_list.entries, todo_list.auto_id, entry)
+
+    %TodoList{todo_list | entries: new_entries, auto_id: todo_list.auto_id + 1}
+  end
+
+  def entries(todo_list, date) do
+    todo_list.entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry end)
+  end
+end
+
+date = ~D[2018-04-29]
+entry = %{date: date, title: "Dentist"}
+entry2 = %{date: date, title: "Football"}
+IO.inspect TodoList.new |>
+TodoList.add_entry(entry) |>
+TodoList.add_entry(entry2) |>
+TodoList.entries(date)

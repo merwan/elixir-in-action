734732a389c8f8571f9f8d680da42e20f099f439
Merouane Atig <merwan@patem.net>
2018-05-17 15:41:57 +0200

    Delete entry

--- todo_crud.ex
+++ todo_crud.ex
@@ -22,6 +22,11 @@ defmodule TodoList do
     end
   end
 
+  def delete_entry(todo_list, entry_id) do
+    new_entries = Map.delete(todo_list.entries, entry_id)
+    %TodoList{todo_list | entries: new_entries}
+  end
+
   def entries(todo_list, date) do
     todo_list.entries
     |> Stream.filter(fn {_, entry} -> entry.date == date end)
@@ -44,4 +49,7 @@ todo_list = TodoList.update_entry(
   &Map.put(&1, :date, ~D[2018-12-20])
 )
 
+IO.inspect todo_list
+
+todo_list = todo_list = TodoList.delete_entry(todo_list, 1)
 IO.inspect todo_list

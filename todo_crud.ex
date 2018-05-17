d384cf28f3f1e8c2a9edd6b7438ee9624f7748f7
Merouane Atig <merwan@patem.net>
2018-05-17 15:39:15 +0200

    Update an entry

--- todo_crud.ex
+++ todo_crud.ex
@@ -11,6 +11,17 @@ defmodule TodoList do
     %TodoList{todo_list | entries: new_entries, auto_id: todo_list.auto_id + 1}
   end
 
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+      {:ok, old_entry} ->
+        new_entry = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
   def entries(todo_list, date) do
     todo_list.entries
     |> Stream.filter(fn {_, entry} -> entry.date == date end)
@@ -21,7 +32,16 @@ end
 date = ~D[2018-04-29]
 entry = %{date: date, title: "Dentist"}
 entry2 = %{date: date, title: "Football"}
-IO.inspect TodoList.new |>
-TodoList.add_entry(entry) |>
-TodoList.add_entry(entry2) |>
-TodoList.entries(date)
+
+todo_list = TodoList.new |> TodoList.add_entry(entry) |>
+TodoList.add_entry(entry2)
+
+IO.inspect todo_list
+
+todo_list = TodoList.update_entry(
+  todo_list,
+  1,
+  &Map.put(&1, :date, ~D[2018-12-20])
+)
+
+IO.inspect todo_list
